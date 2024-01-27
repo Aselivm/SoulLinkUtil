110ede33128f5335c7c6724fec7ba1a8e40796c2
Stepan Primshits <Stepan Primshits>
2024-01-27 22:17:32 +0300

    init

---
+++ SoulLinkUtil.cs
@@ -0,0 +1,47 @@
+﻿using ExileCore;
+using ExileCore.PoEMemory.Components;
+using ExileCore.PoEMemory.MemoryObjects;
+using SharpDX;
+using System;
+
+namespace SoulLinkUtil
+{
+    public class SoulLinkUtil : BaseSettingsPlugin<SoulLinkUtilSettings>
+    {
+        private DateTime lastSoulLinkCastTime = DateTime.MinValue;
+
+        public override bool Initialise()
+        {
+            return true;
+        }
+
+        public override Job Tick()
+        {
+            foreach (var entity in GameController.Entities)
+            {
+                var buffs = entity.Buffs;
+
+                foreach (var buff in buffs)
+                {
+                    if (buff.Name == "soul_link" && buff.Timer < 4 && (DateTime.Now - lastSoulLinkCastTime).TotalSeconds > 1)
+                    {
+                        CastSoulLink();
+                        lastSoulLinkCastTime = DateTime.Now;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private void CastSoulLink()
+    {
+        Input.KeyDown(Settings.CastKey.Value); 
+    }
+
+        public override void Render()
+        {
+            Graphics.DrawText($"Plugin {GetType().Name} is working.", new Vector2(100, 100), Color.Red);
+        }
+    }
+}
---
+++ SoulLinkUtilSettings.cs
@@ -0,0 +1,12 @@
+﻿using ExileCore.Shared.Interfaces;
+using ExileCore.Shared.Nodes;
+
+namespace SoulLinkUtil
+{
+    public class SoulLinkUtilSettings : ISettings
+    {
+        public ToggleNode Enable { get; set; } = new ToggleNode(false);
+        public RangeNode<int> TimeBetweenCasts { get; set; } = new RangeNode<int>(1, 1, 60);
+        public HotkeyNode CastKey { get; set; } = new HotkeyNode(System.Windows.Forms.Keys.F1);
+    }
+}
